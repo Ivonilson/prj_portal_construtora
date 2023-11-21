99b97f69808de16347fad3a7c367c97a2571a85b
Ivonilson <ivonilsoncardoso@hotmail.com>
2023-11-21 18:56:07 -0300

    udpate

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Página Inicial' });
+});
+
+router.get('/new', function(req, res, nex) {
+  res.render('new', { title: 'Página New' });
 });
 
 module.exports = router;
